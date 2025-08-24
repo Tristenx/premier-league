d30b4d6e753d2731a7496c967f7ef6b5b0ef9883
TristenX <1604tristenarron@gmail.com>
2025-08-24 03:48:17 +0100

    Now writes cleaner data to csv

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from selenium.webdriver.remote.webelement import WebElement
 import pandas as pd
 
 
-def get_match_cards(season: int, match_week: int) -> list[dict]:
-    """Returns a list of match cards for a season and match week."""
+def get_match_cards(season: int, match_week: int) -> dict[str, list[str]]:
+    """Returns a dictionary of match data for a season and week."""
     driver = webdriver.Chrome()
     url = f"https://www.premierleague.com/en/matches?competition=8&season={season}&matchweek={match_week}"
     driver.get(url)
@@ -17,21 +17,40 @@ def get_match_cards(season: int, match_week: int) -> list[dict]:
     match_card_elements = match_container.find_elements(
         By.CLASS_NAME, "match-card__info")
 
-    cards = []
-    for element in match_card_elements:
-        new_card = {}
-        teams = element.find_elements(By.CLASS_NAME,
-                                      "match-card__team-name-container")
-        new_card["home_team"] = teams[0].text
-        new_card["away_team"] = teams[1].text
-        new_card["score"] = element.find_element(
-            By.CLASS_NAME, "match-card__score").text
-        cards.append(new_card)
+    cards = {"season": [season for i in range(len(match_card_elements))],
+             "match_week": [match_week for i in range(len(match_card_elements))],
+             "home_team": [i.find_elements(By.CLASS_NAME, "match-card__team-name-container")[0].text for i in match_card_elements],
+             "away_team": [i.find_elements(By.CLASS_NAME, "match-card__team-name-container")[1].text for i in match_card_elements],
+             "score": [i.find_element(By.CLASS_NAME, "match-card__score").text for i in match_card_elements]}
 
     driver.close()
     return cards
 
 
+def file_exists(file_path: str) -> bool:
+    """Checks if a file exists and returns a bool."""
+    try:
+        with open("premier_league_data", "r") as file:
+            pass
+        return True
+    except FileNotFoundError:
+        return False
+
+
+def create_csv_file(file_path: str):
+    """Creates a new empty csv file."""
+    new_csv_df = pd.DataFrame(
+        columns=["season", "match_week", "home_team", "away_team", "score"])
+    new_csv_df.to_csv(path_or_buf=file_path)
+
+
 if __name__ == "__main__":
-    match_cards = get_match_cards(2025, 1)
-    print(match_cards)
+    if not file_exists("premier_league_data.csv"):
+        create_csv_file("premier_league_data.csv")
+    for year in range(2012, 2025):
+        for week in range(1, 39):
+            match_cards = get_match_cards(year, week)
+            match_cards_df = pd.DataFrame(match_cards)
+            match_cards_df.to_csv(
+                path_or_buf="premier_league_data.csv", mode="a", header=False)
+            sleep(1)
