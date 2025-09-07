cb02af2ed93c2f316425be6b81bd50f856e138bf
TristenX <1604tristenarron@gmail.com>
2025-09-08 00:28:23 +0100

    Gets team statistics from team ulr

--- scraper.py
+++ scraper.py
@@ -35,23 +35,41 @@ def get_cl_arguments() -> Namespace:
     return arguments
 
 
-def get_team_stat_links(url: str) -> list[str]:
+def get_team_urls(url: str) -> list[str]:
     """Returns links for each teams statistics."""
     driver = webdriver.Chrome()
     driver.get(url)
     table = driver.find_element(By.ID, "switcher_results2024-202591")
     link_elements = table.find_elements(By.TAG_NAME, "a")
-    team_links = list({element.get_attribute("href") for element in link_elements if re.search(
+    team_urls = list({element.get_attribute("href") for element in link_elements if re.search(
         r"Stats$", element.get_attribute("href"))})
-
     driver.close()
-    return team_links
+    return team_urls
+
+
+def get_team_stats(url: str) -> list[list[str]]:
+    """Returns rows from the team statistics table."""
+    driver = webdriver.Chrome()
+    driver.get(url)
+    table = driver.find_element(
+        By.ID, "all_matchlogs")
+    rows = table.find_elements(By.TAG_NAME, "tr")
+    row_values = []
+    for row in rows:
+        new_row = []
+        date = row.find_element(By.TAG_NAME, "th").text
+        new_row.append(date)
+        values = row.find_elements(By.TAG_NAME, "td")
+        for value in values:
+            new_row.append(value.text)
+        row_values.append(new_row)
+    driver.close()
+    return row_values
 
 
 if __name__ == "__main__":
     args = get_cl_arguments()
     WEBSITE = "https://fbref.com/en/comps/9/2024-2025/2024-2025-Premier-League-Stats"
-    team_stat_links = get_team_stat_links(WEBSITE)
-    print(len(team_stat_links))
-    for link in team_stat_links:
-        print(link)
+    team_links = get_team_urls(WEBSITE)
+    team_stats = get_team_stats(team_links[0])
+    print(team_stats)
